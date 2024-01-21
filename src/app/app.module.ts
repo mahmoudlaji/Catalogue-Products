ded008f982a49eff065a7684bea3cd9cb0e28d06
lajimi mahmoud <mahmoudladjimi@gmail.com>
2024-01-21 22:22:31 +0100

    g all comp service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegistreComponent } from './registre/registre.component';
+import { ProductComponent } from './product/product.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { UpdateComponent } from './update/update.component';
+import { NewProductComponent } from './new-product/new-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistreComponent,
+    ProductComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    UpdateComponent,
+    NewProductComponent
   ],
   imports: [
     BrowserModule,
