55e197f167b5161d39fb1d8ca57fea5d8db995fb
lajimi mahmoud <mahmoudladjimi@gmail.com>
2024-01-21 23:41:15 +0100

    recu app done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegistreComponent } from './registre/registre.component';
-import { ProductComponent } from './product/product.component';
+import { ProductsComponent } from './product/product.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
@@ -10,7 +10,7 @@ const routes: Routes = [
    
    { path: 'login',component :LoginComponent  },
    { path: '',component :RegistreComponent  },
-   { path: 'products', component: ProductComponent },
+   { path: 'products', component: ProductsComponent },
    { path: 'home', component:HomeComponent},
 ];
 
