66a611a70d210ad7d7a7c4a789422f2e07a3b926
lajimi mahmoud <mahmoudladjimi@gmail.com>
2024-01-24 11:21:38 +0100

    use api to add data of users

--- src/app/data-users/data-users.component.ts
+++ src/app/data-users/data-users.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-data-users',
   templateUrl: './data-users.component.html',
   styleUrls: ['./data-users.component.css']
 })
-export class DataUsersComponent {
+export class DataUsersComponent implements OnInit{
+  usersData!:any
+
+  constructor( private apiservice:ApiService) { }
+  ngOnInit(): void {
+  this.usersData = this.apiservice.getAllUsers().subscribe(
+    (result:any)=>{
+      this.usersData = result
+    }
+  )
+  }
 
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,16 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, OnInit } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ApiService {
+export class ApiService implements OnInit {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+  ngOnInit(): void {
+   console.log("Api Service Initialized");
+  }
+ getAllUsers(){
+  return this.http.get('https://jsonplaceholder.typicode.com/users');
+ }
 }
