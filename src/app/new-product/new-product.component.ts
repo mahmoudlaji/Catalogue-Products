3016c1702fe260eaf2111b0e06a07e4505efde3e
lajimi mahmoud <mahmoudladjimi@gmail.com>
2024-01-26 12:31:03 +0100

    fix prob verc deploy 2

--- src/app/new-product/new-product.component.ts
+++ src/app/new-product/new-product.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { ProductService } from '../services/product.service';
 import { Router } from '@angular/router';
-import Swal from 'sweetalert2';
+
 
 @Component({
   selector: 'app-new-product',
@@ -26,14 +26,8 @@ saveProduct(){
 let product = this.newProduct.value
 this.serciceProduct.AddToNewProduct(product)
 this.newProduct.reset()
+alert("Your product are added successufly")
 this.routeP.navigateByUrl("/products")
-Swal.fire({
-  position: 'top-end',
-  icon: 'success',
-  title: 'Your product has been add!!',
-  showConfirmButton: false,
-  timer: 1500
-})
 }
 backProduct(){
   this.routeP.navigateByUrl("/products")
